128b8eb2cad1e504a7c20a76e0bbde4f56fb4ae3
Isaac Ramirez <iramirezc@live.com.mx>
2022-04-03 16:43:07 -0500

    Add Button stories.

---
+++ examples/with-react-flow-webpack/src/components/Button/Button.stories.js
@@ -0,0 +1,21 @@
+// @flow
+import React from "react";
+
+import Button from "./index";
+
+export default {
+  title: "Examples/Button",
+  component: Button,
+};
+
+const handleClick = () => console.log("Click!");
+
+export const Primary = () => <Button title="Primary" onClick={handleClick} />;
+
+export const Warning = () => (
+  <Button title="Warning" color="warning" onClick={handleClick} />
+);
+
+export const Error = () => (
+  <Button title="Error" color="error" onClick={handleClick} />
+);
